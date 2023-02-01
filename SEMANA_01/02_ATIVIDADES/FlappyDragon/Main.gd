2eecd7199dabf299bc2eabc41b14aafea074269b
EverFelliphe <123915570+EverFelliphe@users.noreply.github.com>
2023-02-01 20:39:56 -0300

    atividade 1

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
-var status = 1
-var vscore = 0
-var x = 1.5 
-var y = 1.5 
+var status = 1 # variavel de controle responsavel pela continuação do game, ou seja se o player perdeu ou não.
+var vscore = 0 #variavel responsavel por armazenar o score do player ao ultrapassar as colunas
+var x = 4 # variavel de controle referente ao eixo x do plano responsavel pelo movimento do cenario de fundo
+var y = 1.5 # variavel de controle referente ao eixo y do plano responsavel pela queda do dragão no inicio do game
 
 # executa essa função ao carregar o jogo
 func _ready():
@@ -41,11 +41,11 @@ func _process(delta):
 			
 		# se apertou seta para baixo, aumenta o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_down"):
-			$dragon.position.y += 2
+			$dragon.position.y += 7
 
 		# se apertou seta para cima, diminui o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_up"):
-			$dragon.position.y -= 4
+			$dragon.position.y -= 7
 			
 	elif status == 0: # parado
 		
